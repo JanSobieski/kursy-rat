29194448229cb7d781e21c1117701190e8924005
wdro <wdro@wd-w540.site>
2016-12-18 12:17:52 +0100

    c

--- kursy-rat/src/main/java/pl/wd/kursy/data/BasicType.java
+++ kursy-rat/src/main/java/pl/wd/kursy/data/BasicType.java
@@ -6,6 +6,10 @@ public class BasicType implements Serializable,Cloneable {
 	private static final long serialVersionUID = -3087968737693385232L;
 
 	protected int _id;
+	private boolean _editable;
+	private boolean _selected;
+	private boolean _selected_neg;
+	protected String _name;
 	
 	public BasicType() {
 	}
@@ -14,4 +18,64 @@ public class BasicType implements Serializable,Cloneable {
 		_id = id;
 	}
 
+	public int get_id() {
+		return _id;
+	}
+
+	public void set_id( int id ) {
+		_id = id;
+	}
+	
+	public String getName() {
+		if ( _name == null ) {
+			return "";
+		}
+		return _name;
+	}
+
+	public void setName( String name ) {
+		_name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		if ( _id == 0 ) {
+			return super.hashCode();
+		}
+		
+		return _id;
+	}
+
+	@Override
+	public boolean equals( Object obj ) {
+		if ( obj == null ) {
+			return false;
+		}
+		if ( obj instanceof BasicType ) {
+			if ( _id == ((BasicType) obj).get_id() ) {
+				if ( _id == 0 ) {
+					return super.equals( obj );
+				}
+				return true;
+			}
+			else
+				return false;
+		}
+		return false;
+	}
+
+	public boolean is_selected() {
+		return _selected;
+	}
+
+	public void set_selected( Object selected ) {
+		if ( selected == null ) {
+			_selected = false;
+			return;
+		}
+		if ( selected instanceof Boolean ) {
+			_selected = ((Boolean)selected).booleanValue();
+		}
+	}
+	
 }
