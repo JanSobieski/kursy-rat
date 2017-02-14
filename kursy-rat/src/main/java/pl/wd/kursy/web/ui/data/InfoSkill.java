1d61f7b78d06ed5cf364ca6f0201e80ddff02ab8
wdro <wdro@wd-w540.site>
2017-02-14 08:49:27 +0100

    c

---
+++ kursy-rat/src/main/java/pl/wd/kursy/web/ui/data/InfoSkill.java
@@ -0,0 +1,23 @@
+package pl.wd.kursy.web.ui.data;
+
+public class InfoSkill {
+	private String _text;
+	private int _row;
+
+	public String getText() {
+		return _text;
+	}
+
+	public void setText(String text) {
+		_text = text;
+	}
+
+	public int getRow() {
+		return _row;
+	}
+
+	public void setRow(int row) {
+		_row = row;
+	}
+
+}
